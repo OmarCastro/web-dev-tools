4b842e3ed607d4d9a2e7bc3db7b47e9c3b9d34a9
Omar Castro <omar.castro.360@gmail.com>
2023-01-16 23:33:27 +0000

    src: add unit test api to urn tests in Deno and browser

---
+++ src/browser-client/test-utils/unit-test-api.ts
@@ -0,0 +1,27 @@
+let testMethod: (description: string, testFn: () => void | Promise<void>) => void
+if("Deno" in globalThis){
+    testMethod = Deno.test
+} else {
+
+    
+
+    const testList: {description: string, testFn: () => void | Promise<void>}[] = []
+    let running = false
+    const execute = async function(){
+        if(running){ return }
+        running = true
+        while(testList.length > 0){
+            await testList[0].testFn()
+        }
+        running = false
+    }
+
+    testMethod = (description: string, testFn: () => void | Promise<void>) => {
+        testList.push({description, testFn})
+        execute();
+    }  
+  }
+  
+  export const test = testMethod 
+  export { assertEquals } from "https://deno.land/std@0.172.0/testing/asserts.ts"
+  
