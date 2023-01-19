363885c8852332316e76f96b11a63bfd0b334f54
Omar Castro <omar.castro.360@gmail.com>
2023-01-19 01:49:24 +0000

    build: add test and test-watch scripts

---
+++ buildtools/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd SCRIPT_DIR/..
+deno test --allow-read=src/ --no-check --allow-net --allow-env --import-map=src/browser-client/test-utils/import_map.json  src/browser-client/unit-tests.spec.ts
