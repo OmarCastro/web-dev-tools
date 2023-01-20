5538288cd0fd8b77906301f4490d7c7a4cae4983
Omar Castro <omar.castro.360@gmail.com>
2023-01-20 22:10:57 +0000

    fix test & test-watch build scripts cd parameter

--- buildtools/test-watch.sh
+++ buildtools/test-watch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd SCRIPT_DIR/..
+cd "$SCRIPT_DIR"/..
 deno test --watch --allow-read=src/ --no-check --allow-net --allow-env --import-map=src/browser-client/test-utils/import_map.json  src/browser-client/unit-tests.spec.ts
