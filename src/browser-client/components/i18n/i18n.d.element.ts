e65d7bea0e5888bdb8ac5f6cee6de5b62854ee97
Omar Castro <omar.castro.360@gmail.com>
2023-01-21 20:16:48 +0000

    src: second iteration of i18n component
    - use dynamic loading util and extend it

---
+++ src/browser-client/components/i18n/i18n.d.element.ts
@@ -0,0 +1,3 @@
+import {registerComponent} from '../../utils/web-components/dynamic-loading-element.util'
+registerComponent("./i18n.element.js", import.meta.url)
+  
--- src/browser-client/components/i18n/i18n.element.ts
+++ src/browser-client/components/i18n/i18n.element.ts
@@ -1,11 +1,31 @@
-class I18nElement extends HTMLElement {
-    get key(){
-        return this.getAttribute("key")
-    }
 
-    set key(key: string) {
-        this.setAttribute("key", key)
-    }
+const initiated = Symbol();
+
+
+const addProperty = (propertyName: string ,clazz: typeof HTMLElement) => {
+    Object.defineProperty(clazz.prototype, propertyName, {
+        get: function(this: HTMLElement) {
+            return this.getAttribute(propertyName)
+        },
+        
+        set: function(this: HTMLElement, key: string) {
+            this.setAttribute(propertyName, key)
+        }
+        
+    });
+
 }
 
-customElements.define("x-i18n", I18nElement)
+function extendClass(clazz: typeof HTMLElement){
+    addProperty("key", clazz)
+}
+
+
+export function apply<T extends HTMLElement>(instance: T, clazz: typeof HTMLElement){
+
+    if(!clazz.prototype[initiated]){
+        extendClass(clazz)
+        clazz.prototype[initiated] = true
+
+    }
+}
